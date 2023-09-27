eb7d0b5e079eda06d78ec9e9c749559c2087e855
Rodrigo Amor <65867883+rodrigoamor5@users.noreply.github.com>
2023-09-26 22:43:02 -0300

    Add files via upload

---
+++ RECUPERATORIO-MAIN-2023/src/componentes/Item.js
@@ -0,0 +1,21 @@
+// CONSIGNAS EN ITEM:  🚩
+// 1- crear un <li> que tenga su click preparado para ejecutar el metodo heredado para seleccionar la plataforma como favorita
+// 2- Presentar el nombre de la plataforma que le llega como prop
+
+// El componente Item:
+// ESTADO: Item no necesita manejar un estado.
+// MÉTODOS: Item no requiere de métodos.
+// PROPS: Item recibe como props el método para modificar la plataforma favorita y su respectivo nombre como contenido.
+
+export default function Item(props) {
+    return (
+      <>
+      <li
+        onClick={() =>props.handleClick(props.nombre)}
+      >
+        {props.nombre}
+      </li>
+      </>
+    );
+  }
+  
